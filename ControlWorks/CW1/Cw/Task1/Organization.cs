39e87a78ba084bd6f28f35bd223eea336d52aa4a
Flekseyy <aleksejsmolaninov40@gmail.com>
2026-04-11 15:38:18 +0300

    Написал тест

---
+++ ControlWorks/CW1/Cw/Task1/Organization.cs
@@ -0,0 +1,26 @@
+﻿namespace Cw;
+
+public class Company
+{
+    public string Name { get; set; }
+}
+
+public class Worker
+{
+    public string Name { get; set; }
+    public Company Employer { get; set; }
+}
+
+public class Organization<T> where T : Worker, new()
+{
+    
+    public T HireReplacement(T oldWorker, Company newCompany)
+    {
+        return new T
+        {
+            Name = oldWorker.Name,    
+            Employer = newCompany
+        };
+        
+    }
+}
---
+++ ControlWorks/CW1/Cw/Task2/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Task2;
+
+public class Car
+{
+    public string Model { get; set; }
+    public int CurrentSpeed { get; private set; }
+    public event EventHandler<int> SpeedExceeded;
+
+    public void Accelerate(int amount)
+    {
+        CurrentSpeed += amount;
+        if (CurrentSpeed >= 110)
+        {
+            SpeedExceeded?.Invoke(this, CurrentSpeed);
+        }
+        
+    }
+}
